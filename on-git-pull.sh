bed60c6c92a5046c3aca077219ffa585b25051d7
kornel.zemla <kornel.zemla@ocado.com>
2016-12-21 18:42:05 +0100

    Logs

--- on-git-pull.sh
+++ on-git-pull.sh
@@ -9,16 +9,16 @@ function log {
 RELEASE_DIR=~/var/remote-heater-server
 
 log "About to kill <<Mongo DB>>"
-kill $(ps aux | grep -v "grep" | grep "mongod" | awk '{print $2}')
+kill $(ps aux | grep -v "grep" | grep "mongod" | awk '{print $2}') | tee -a ${LOG_FILE} 2>&1
 
 log "About to kill <<Remote Heater Server>>"
-kill $(ps aux | grep -v "grep" | grep "node main.js" | awk '{print $2}')
+kill $(ps aux | grep -v "grep" | grep "node main.js" | awk '{print $2}') | tee -a ${LOG_FILE} 2>&1
 
 log "About to build new version of <<Remote Heater Server>> in ${RELEASE_DIR}"
-./build-release.sh ${RELEASE_DIR}
+./build-release.sh ${RELEASE_DIR} | tee -a ${LOG_FILE} 2>&1
 
 log "About to run <<Mongo DB>>"
-~/bin/mongod-start.sh
+~/bin/mongod-start.sh | tee -a ${LOG_FILE} 2>&1
 sleep 3
 
 log "About to run <<Remote Heater Server>>"
