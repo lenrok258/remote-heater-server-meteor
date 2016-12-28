f95417a8ed9836f78b4831a771cc60ba62a647b8
kornel.zemla <kornel.zemla@ocado.com>
2016-12-28 17:55:41 +0100

    Temperature chart

--- imports/ui/heater-status.js
+++ imports/ui/heater-status.js
@@ -1,5 +1,7 @@
 import './heater-status.html'
-import {HeaterStatus} from '../api/heater-status.js'
+import {
+    HeaterStatus
+} from '../api/heater-status.js'
 
 Template.heaterStatus.onCreated(function bodyOnCreated() {
     Meteor.subscribe('heater-status');
@@ -35,17 +37,84 @@ Template.heaterStatus.helpers({
     formatTemperature(tempToFormat) {
         return `${tempToFormat}&#x2103;`;
     },
-    
+
     formatStatus(statusToFormat) {
-        switch(statusToFormat) {
+        switch (statusToFormat) {
             case 'on':
                 return 'włączony';
             case 'off':
                 return 'wyłączony';
             default:
                 return 'nieznany';
-            
+
         }
+    },
+
+    temperatureChartData() {
+        let dataInput = HeaterStatus.find({}, {
+            sort: {
+                createdAt: -1
+            },
+            limit: 120
+        }).fetch();
+
+        let dataChart = dataInput.map(function (input) {
+            return {
+                x: input.createdAt,
+                y: input.temperature
+            }
+        })
+
+        return {
+            chart: {
+                zoomType: 'x'
+            },
+            title: {
+                text: 'Temperatura w biurze'
+            },
+            xAxis: {
+                type: 'datetime'
+            },
+            yAxis: {
+                title: {
+                    text: 'Temperatura'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                area: {
+                    fillColor: {
+                        linearGradient: {
+                            x1: 0,
+                            y1: 0,
+                            x2: 0,
+                            y2: 1
+                        },
+                        stops: [
+                            [0, Highcharts.getOptions().colors[0]],
+                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                        ]
+                    },
+                    marker: {
+                        radius: 2
+                    },
+                    lineWidth: 1,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    threshold: null
+                }
+            },
+
+            series: [{
+                type: 'area',
+                data: dataChart
+            }]
+        };
     }
 
 });
